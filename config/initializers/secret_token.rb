417510f278eb575006516ce2d556321e6f381dc4
GARUM Fundatio <juantomas@lared.es>
2010-12-05 13:56:59 +0100

    generada la aplicación rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bazarserver::Application.config.secret_token = '690b97b925b416161d19b1ea4c599e7fd901704fabf266f5d036be185dc1c2a306a70165e0a60a4d2fbe849c04f5dad11a033f176e89a8261f1fea84a1863d1f'
