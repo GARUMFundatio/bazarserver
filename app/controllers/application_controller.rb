05e3d15969f28f18f6315dc1df57e385bc9b957d
GARUM Fundatio <juantomas@lared.es>
2010-12-19 14:40:15 +0100

    incluyendo BZ_param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :BZ_param
+  
+  def BZ_param(clave)
+    conf = Conf.find_by_nombre(clave)
+    return conf.valor || nil   
+  end
+  
 end
