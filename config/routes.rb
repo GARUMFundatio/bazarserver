735ef1d84e876fba75259a529c563a49e753056d
root <root@rails.(none)>
2010-12-19 14:06:32 +0000

    incluido devise para autentificacion

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Bazarserver::Application.routes.draw do
   
+  devise_for :users, :path => "usuarios", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
   resources :clusters
   resources :gruposconfs
   resources :confs
