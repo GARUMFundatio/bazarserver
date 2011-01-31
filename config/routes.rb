7c96b0dbd9daecef6f5be3d627fa359af3655a87
GARUM Fundatio <juantomas@lared.es>
2011-01-31 12:49:59 +0100

    localizando el login

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Bazarserver::Application.routes.draw do
   
-  devise_for :users, :path => "usuarios", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
+  root :to => 'home#index'
+  match '/noticias.rss' => 'noticias#rss'
+  
+  devise_for :users, :path => "usuarios", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register'} #, :sign_up => 'cmon_let_me_in' }
   resources :clusters
   resources :gruposconfs
   resources :confs
   resources :noticias
   
-  root :to => 'home#index'
+  
 
   match 'micuenta' => "user#edit",          :as => :micuenta
   match 'login' => "devise/sessions#new",      :as => :login
